5dbcf6a4ddd1494d067c581bfcacaacc19c377a6
Rafay <arafaymbir@gmail.com>
2020-07-23 17:02:20 -0400

    added a router file

--- src/router/index.js
+++ src/router/index.js
@@ -10,29 +10,11 @@ Vue.use(VueRouter)
     name: 'Home',
     component: Home
   },
+  
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: '/grocery',
-    name: 'Grocery',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Grocery.vue')
-  },
-  {
-    path: '/rafay',
-    name: 'Rafay',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Rafay.vue')
+    path: '/todo-list',
+    name: 'TodoList',
+    component: () => import('../views/Todo-list.vue')
   }
 ]
 
