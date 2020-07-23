92331cdf281ffbe6b4e7e9046c5a95ecdf1d1cb0
Rafay <arafaymbir@gmail.com>
2020-07-23 15:56:10 -0400

    made suggested changes

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/grocery">Grocery</router-link> |
-      <router-link to="/rafay">Rafay</router-link>
+      <router-link to="/todo-list">Todo List</router-link>
     </div>
     <router-view/>
   </div>
